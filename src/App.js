a890686def6d0455e3d9f2b1657fe75d0952d08c
paul <paulolutosoye@gmail.com>
2022-08-28 18:22:22 +0100

    background-image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import useFetch from './components/useFetch';
+
 
 function App() {
+
+  const { data, loading, error, refetch } = useFetch(
+    "https://randomuser.me/api/?gender=female"
+  );
+
+  if (loading) return <h1> LOADING...</h1>;
+
+  if (error) console.log(error);
+  console.log(data)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='card'>
+
+      <img className="gender" src={data?.results[0].picture.large} alt=''/>
+      <span>{data?.results[0].name.title}</span>
+      <div className='names'>
+      <h1>{data?.results[0].name.first}  {data?.results[0].name.last}</h1>
+      </div>
+      
+
+      <p>{data?.results[0].gender}</p>
+
+      <div class="buttons">
+      <i class="fa fa-envelope" aria-hidden="true"></i>
+            <h6>{data?.results[0].email}</h6>
+        </div>
+
+        <div class="buttons">
+      <i class="fa fa-phone" aria-hidden="true"></i>
+            <h6>{data?.results[0].phone}</h6>
+        </div>
+        <div class="buttons">
+        <i class="fa fa-map-marker" aria-hidden="true"></i>
+            <h6>{data?.results[0].location.country}</h6>
+        </div>
+    </div>
+      <button  className='next' onClick={refetch}>New user</button> 
     </div>
   );
 }
